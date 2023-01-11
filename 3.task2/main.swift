07cc3dc01227347b202f87491cc8bad15b665bcb
imrannn24 <imran200424@icloud.com>
2023-01-11 14:16:51 +0600

    dz

--- 3.task2/main.swift
+++ 3.task2/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Введите текст:")
 
+let text = readLine()!
+
+print("Введите букву:")
+
+if let bukva = readLine(){
+    let b = Character(bukva)
+    var a = 0
+    
+    for i in text{
+        if i == b{
+            a += 1
+        }
+    }
+    print("В тексте буква '\(b)' встречается: \(a) раз ")
+}
